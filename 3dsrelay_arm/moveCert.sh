41e8edf497fede728e79b87f9d0c3d0ee55e1dc0
Anastasia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-06-26 18:56:11 -0700

    Update and rename moveCert.sh to startDocker.sh

--- 3dsrelay_arm/moveCert.sh
+++
@@ -1,4 +0,0 @@
-openssl x509 -in /var/lib/waagent/$1.crt -out /etc/ssl/certs/turn/turn_server_cert.pem
-cp /var/lib/waagent/$1.prv  /etc/ssl/certs/turn/turn_server_pkey.pem
-
-docker run -v /etc/ssl/certs/turn:/etc/ssl -d -p 3478:3478 -p 3478:3478/udp --restart=always $2 $3 $4
---
+++ 3dsrelay_arm/startDocker.sh
@@ -0,0 +1,7 @@
+sudo mkdir /etc/ssl/certs/turn
+
+sudo openssl x509 -in /var/lib/waagent/$1.crt -out /etc/ssl/certs/turn/turn_server_cert.pem
+
+sudo cp /var/lib/waagent/$1.prv  /etc/ssl/certs/turn/turn_server_pkey.pem
+
+sudo docker run -v /etc/ssl/certs/turn:/etc/ssl -d -p 3478:3478 -p 3478:3478/udp --restart=always $2 $3 $4
