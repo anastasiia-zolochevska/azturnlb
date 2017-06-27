d6e5b7127019003e40a622ea3b7b215941661296
Anastasia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-06-26 21:26:36 -0700

    Update startDocker.sh

--- 3dsrelay_arm/startDocker.sh
+++ 3dsrelay_arm/startDocker.sh
@@ -4,4 +4,4 @@ sudo openssl x509 -in /var/lib/waagent/$1.crt -out /etc/ssl/certs/turn/turn_serv
 
 sudo cp /var/lib/waagent/$1.prv  /etc/ssl/certs/turn/turn_server_pkey.pem
 
-sudo docker run -v /etc/ssl/certs/turn:/etc/ssl -d -p 3478:3478 -p 3478:3478/udp --restart=always $2 $3 $4
+sudo docker run -v /etc/ssl/certs/turn:/etc/ssl -d 3478:3478 -p 3478:3478/udp -p 5349:5349 -p 5349:5349/udp --restart=always $2 $3 $4
