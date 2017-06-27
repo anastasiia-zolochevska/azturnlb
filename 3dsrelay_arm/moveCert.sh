38e48193e5c23f7388a736e3be697bb147c5ddb0
Anastasia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-06-26 17:49:17 -0700

    Update moveCert.sh

--- 3dsrelay_arm/moveCert.sh
+++ 3dsrelay_arm/moveCert.sh
@@ -1,2 +1,4 @@
 openssl x509 -in /var/lib/waagent/$1.crt -out /etc/ssl/certs/turn/turn_server_cert.pem
 cp /var/lib/waagent/$1.prv  /etc/ssl/certs/turn/turn_server_pkey.pem
+
+docker run -v /var/lib/waagent:/etc/ssl -d -p 3478:3478 -p 3478:3478/udp --restart=always $2 $3 $4
