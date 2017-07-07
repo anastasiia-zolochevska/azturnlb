d5716948a673f4f7115c650e2214d873970a30e5
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-07-06 17:49:37 -0700

    Use shared secret for turnadmin instead of username/password

--- dbsetup/add_turnuser.sh
+++ dbsetup/add_turnuser.sh
@@ -3,16 +3,15 @@
 set -e
 
 if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
-    echo "Usage: $0 <postgres_connection_string> <username> <password> <realm>"
+    echo "Usage: $0 <postgres_connection_string> <sharedSecret> <realm>"
     exit 1
 fi
 
 set -u
 psqlConnectionString="$1"
-userToAdd="$2"
-userPassword="$3"
-realm="$4"
+sharedSecret="$2"
+realm="$3"
 
-docker run --rm obsoleted/3drelayadmin:v0.0.1a -e "$psqlConnectionString" -a -u $userToAdd -p $userPassword -r $realm
+docker run --rm zolochevska/3drelayadmin:latest -e "$psqlConnectionString" -a -s $sharedSecret -r $realm
 
 echo Done
