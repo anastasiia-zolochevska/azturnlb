09e11aa0fc3376a3a0a82006babb4a5a68fd6b66
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-07-10 14:40:45 -0700

    Add script that creates shared secret

---
+++ dbsetup/add_turnsecret.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+    echo "Usage: $0 <postgres_connection_string> <secret> <realm> <relayadmin_docker_image>"
+    exit 1
+fi
+
+set -u
+psqlConnectionString="$1"
+secret="$2"
+realm="$3"
+dockerimage=${4:-"zolochevska/3dsrelayadmin:latest"}
+
+docker run --rm $dockerimage -e "$psqlConnectionString" -a -s $secret -r $realm
+
+echo Done
--- dbsetup/add_turnuser.sh
+++ dbsetup/add_turnuser.sh
@@ -3,15 +3,17 @@
 set -e
 
 if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
-    echo "Usage: $0 <postgres_connection_string> <sharedSecret> <realm>"
+    echo "Usage: $0 <postgres_connection_string> <username> <password> <realm> <relayadmin_docker_image>"
     exit 1
 fi
 
 set -u
 psqlConnectionString="$1"
-sharedSecret="$2"
-realm="$3"
+userToAdd="$2"
+userPassword="$3"
+realm="$4"
+dockerimage=${5:-"zolochevska/3dsrelayadmin:latest"}
 
-docker run --rm zolochevska/3drelayadmin:latest -e "$psqlConnectionString" -a -s $sharedSecret -r $realm
+docker run --rm  $dockerimage -e "$psqlConnectionString" -a -u $userToAdd -p $userPassword -r $realm
 
 echo Done
